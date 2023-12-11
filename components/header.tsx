1042a04095a9b6012a39f9c11199c6abf5270ab6
vietthai244 <thai.toviet@gmail.com>
2023-12-11 20:42:29 +0700

    Update header styles

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,16 @@
 import barchartIcon from '@/assets/icons/bar-chart.svg'
 import logo from '@/assets/icons/logo.svg'
 import walletIcon from '@/assets/icons/wallet.svg'
+import discordIcon from '@/assets/icons/discord.svg'
+import twitterIcon from '@/assets/icons/twitter.svg'
+import messengerIcon from '@/assets/icons/messenger.svg'
 import Image from 'next/image'
 
 export default function MainHeader() {
   return (
     <nav className="main-header flex items-center justify-between">
       <div className="flex-1 text-subtitle-1">
-        <button className="base-button flex items-center gap-2">
+        <button className="base-button clip-right flex items-center gap-2">
           <Image src={barchartIcon} alt="menu" />
           Menu
         </button>
@@ -22,7 +25,17 @@ export default function MainHeader() {
       </div>
 
       <div className="flex-1 flex items-center justify-end gap-4">
-        <button className="base-button flex items-center gap-2 text-subtitle-1">
+        <div className="icon-wrapper">
+          <Image src={discordIcon} alt="discord" />
+        </div>
+        <div className="icon-wrapper">
+          <Image src={twitterIcon} alt="twitter" />
+        </div>
+        <div className="icon-wrapper">
+          <Image src={messengerIcon} alt="facebook" />
+        </div>
+
+        <button className="base-button clip-left flex items-center gap-2 text-subtitle-1">
           <Image src={walletIcon} alt="menu" />
           Connect
         </button>
